0b8a428d82dcbf783d037966449e9813e5956d00
amutamil <68475039+amutamil@users.noreply.github.com>
2020-10-09 22:43:12 +0530

    Add files via upload

---
+++ DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Base64.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Base64.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Clients.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Clients.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Heap.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Heap.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/LinkedList.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/LinkedList.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Log.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Log.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTPacket.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTPacket.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTPacketOut.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTPacketOut.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTPersistence.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTPersistence.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTPersistenceDefault.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTPersistenceDefault.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTProperties.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTProperties.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTProtocolClient.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTProtocolClient.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTProtocolOut.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTProtocolOut.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTReasonCodes.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTReasonCodes.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/MQTTTime.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/MQTTTime.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Messages.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Messages.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/SHA1.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/SHA1.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Socket.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Socket.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/SocketBuffer.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/SocketBuffer.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/StackTrace.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/StackTrace.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Thread.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Thread.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/Tree.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/Tree.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/WebSocket.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/WebSocket.c.obj"
+  "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/src/utf-8.c" "C:/Users/Amuthamil Kavioviyan/Desktop/Philips/case study 2/paho/paho.mqtt.c/build.paho/src/CMakeFiles/common_obj.dir/utf-8.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "PAHO_MQTT_EXPORTS=1"
+  "WIN32_LEAN_AND_MEAN"
+  "_CRT_SECURE_NO_DEPRECATE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
